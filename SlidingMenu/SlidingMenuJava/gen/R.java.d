be5e00f6c1120c040cc61bcfe8f384c0a241ed29
Tom Opgenorth <tom@opgenorth.net>
2013-01-10 22:04:10 -0700

    Adding in java source, ActionBarSherlock binding, and a binding for SlidingMenu

---
+++ SlidingMenu/SlidingMenuJava/gen/R.java.d
@@ -0,0 +1,13 @@
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/gen/com/slidingmenu/lib/R.java \
+ : /Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/drawable/defaultshadow.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/drawable/defaultshadowright.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/layout/list.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/layout/shadow.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/layout/slidingmenumain.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/layout/slidingmenumainlist.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/layout/slidingmenurow.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/values/attrs.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/values/colors.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/values/dimen.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/res/values/ids.xml \
+/Users/tom/work/xamarin/code/monodroid-samples/SlidingMenu/SlidingMenuJava/bin/AndroidManifest.xml \
